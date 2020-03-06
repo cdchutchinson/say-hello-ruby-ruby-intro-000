7dbc878a46c434342ffc1d0b533a22988b31f865
Crystal <cdchutchinson@gmail.com>
2020-03-06 04:13:18 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
+=======
+def say_Hello(name1)
+  "say_hello #{name1}"
+end
+say_hello("Kent Beck")
+
+def say_hello(name2="Ruby programmer")
+  "say_hello #{name 2}"
+>>>>>>> 1b93238966cff744bf6ed05b84f601952fb81cec
 end
